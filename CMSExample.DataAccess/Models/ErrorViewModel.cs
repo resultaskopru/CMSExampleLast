aca6cce03736ced2168cd4d1bba296dfa9d1f434
resul <resultaskpru@gmail.com>
2024-08-15 18:20:32 +0300

    refactor

--- CMSExample.DataAccess/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CMSExample.DataAccess.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
